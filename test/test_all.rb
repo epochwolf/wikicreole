6f5e0199db22d83a9998c246b3b511f1e00416ec
Epoch Wolf <epochwolf@gmail.com>
2010-01-04 14:43:09 -0600

    Adding comment to test file to explain changes

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'test/unit'
-require 'lib/wiki_creole'
+require 'lib/wiki_creole' #test wouldn't run with previous path? (Probably my fault)
 
 class TC_WikiCreole < Test::Unit::TestCase
 
